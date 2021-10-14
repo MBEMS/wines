2ac63c50d62a448891a01d29a7f7755850ecccdc
MBEMS <92099235+MBEMS@users.noreply.github.com>
2021-10-14 08:30:22 +0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# wines
-lesson
+# myFirstDesign
---
+++ idex.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper', {    
+    loop: true,
+  
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+      
+    },
+  
+    
+    
+  });
+  
